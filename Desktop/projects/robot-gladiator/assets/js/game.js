d7a8a7b6177b344fc6f400e3ac1bae838c3536e8
Olivia Faraone <oliviafaraone1@MacBook-Pro-10.local>
2020-12-09 20:40:18 -0700

    start and end game functions

--- Desktop/projects/robot-gladiator/assets/js/game.js
+++ Desktop/projects/robot-gladiator/assets/js/game.js
@@ -72,12 +72,17 @@ var fight = function(enemyName) {
     }
 };
 
+var startGame = function (){ 
+    // reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
 
     for (var i = 0; i < enemyNames.length; i++) {
         if (playerHealth > 0) {
             // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
             window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-          }
+        
             // pick new enemy to fight based on the index of the enemyNames array  
             var pickedEnemyName=enemyNames[i];
 
@@ -89,7 +94,41 @@ var fight = function(enemyName) {
 
         // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
         fight(pickedEnemyName);
+        } 
+        else {
+            window.alert ("You have lost your robot battle! Game Over!");
+            break;
+        }
+        }
+        // after the loop ends, player is either out of health or enemies to fight, so run the endGame function
+      endGame();
       };
+
+
+ // function to end the entire game
+var endGame = function() {
+      // if player is still alive, player wins!
+  if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+  } 
+  else {
+    window.alert("You've lost your robot in battle.");
+  }
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+
+if (playAgainConfirm) {
+  // restart the game
+  startGame();
+} 
+else {
+  window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+}
+};
+
+// ask player if they'd like to play again
+
+      // start the game when the page loads
+        startGame();
  
 
 // Game States
